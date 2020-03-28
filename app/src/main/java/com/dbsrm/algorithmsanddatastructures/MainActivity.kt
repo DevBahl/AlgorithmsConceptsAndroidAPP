1d61bc1affc0510237eb6e54db4a07c8b7c10cdb
Dev Bahl <devbahl12@gmail.com>
2020-03-29 01:05:14 +0530

    second commit

--- app/src/main/java/com/dbsrm/algorithmsanddatastructures/MainActivity.kt
+++ app/src/main/java/com/dbsrm/algorithmsanddatastructures/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dbsrm.algorithmsanddatastructures
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Algorithms::class.java)
             startActivity(intent)
         }
+
+        button.setOnClickListener {
+            Toast.makeText(this,"Still Working On it",Toast.LENGTH_LONG).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this,"Still Working On it",Toast.LENGTH_LONG).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this,"Still Working On it",Toast.LENGTH_LONG).show()
+        }
+
+        var str = "Right Now Only Algorithm Concepts are added, I am Working on others"
+        displayText.setText(str)
+
+
     }
 }
